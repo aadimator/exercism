c868d97f50e3dd0da6ffa3143bacebcff762882f
Aadam <aadimator@gmail.com>
2020-11-20 14:12:53 +0500

    complete hamming exercise

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,5 +1,19 @@
+#include <stdexcept>
 #include "hamming.h"
 
+using namespace std;
+
 namespace hamming {
+
+    int compute(const string &a, const string &b) {
+        if (a.length() != b.length())
+            throw domain_error("Strings should be of equal length.");
+
+        int distance = 0;
+        for (size_t i = 0; i < a.length(); i++) {
+            if (a[i] != b[i]) ++distance;
+        }
+        return distance;
+    }
 
 }  // namespace hamming
--- cpp/hamming/hamming.h
+++ cpp/hamming/hamming.h
@@ -1,7 +1,11 @@
+#include <string>
+
 #if !defined(HAMMING_H)
 #define HAMMING_H
 
 namespace hamming {
+
+    int compute(const std::string &a, const std::string &b);
 
 }  // namespace hamming
 
