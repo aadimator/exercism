cdf975ab6e6a9510e7fb3d352978d28253ed4e68
Aadam <aadimator@gmail.com>
2021-02-20 12:15:37 +0500

    complete nucleotide

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -7,5 +7,9 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    d = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    for c in strand
+        try d[c] += 1 catch; throw(DomainError("Invalid strand")) end
+    end
+    return d
 end
